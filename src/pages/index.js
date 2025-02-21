7bd74c61be38c97201af959a4f0e0447a5e9bde1
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2025-02-21 14:41:13 +0700

    feat induce error 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            Yitzhak Edmund Tio Manalu | 499769
           </h5>
         </div>
       </main>
